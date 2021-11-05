02a2d7f11862f8220d5a2b93e80ee834d84e3ff5
Nikolui333 <semenovnikolu@yandex.ru>
2021-11-05 18:27:50 +0300

    ActivityMainBinding

--- app/src/main/java/com/example/widgetsproject/MainActivity.kt
+++ app/src/main/java/com/example/widgetsproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.widgetsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import com.example.widgetsproject.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
