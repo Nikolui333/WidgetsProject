f861643c56ab2dd56c8f611537df20c426ca357c
Nikolui333 <semenovnikolu@yandex.ru>
2021-11-08 20:15:05 +0300

    Bottom Navigation upgrade

--- app/src/main/java/com/example/widgetsproject/MainActivity.kt
+++ app/src/main/java/com/example/widgetsproject/MainActivity.kt
@@ -7,26 +7,31 @@ import androidx.databinding.DataBindingUtil
 import com.example.widgetsproject.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
-    private var binding : ActivityMainBinding? = null
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         supportFragmentManager.beginTransaction().replace(R.id.content, Shop()).commit()
 
-        binding?.bottomNav?.setOnNavigationItemSelectedListener(this)
-        binding?.bottomNav?.selectedItemId = R.id.shopItemBottomNav
-    }
+        binding?.bottomNav?.setOnItemSelectedListener { item ->
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.baselineMenuBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, BaselineMenu()).commit()
-            R.id.shopItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Shop()).commit()
-            R.id.deliveryItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Delivery()).commit()
-            R.id.accountItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Account()).commit()
+            when (item.itemId) {
+                R.id.baselineMenuBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, BaselineMenu()).commit()
+                R.id.shopItemBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, Shop()).commit()
+                R.id.deliveryItemBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, Delivery()).commit()
+                R.id.accountItemBottomNav -> supportFragmentManager.beginTransaction()
+                    .replace(R.id.content, Account()).commit()
+            }
+
+            return@setOnItemSelectedListener true
+
         }
-        return true
+        binding?.bottomNav?.selectedItemId = R.id.shopItemBottomNav
     }
 }
